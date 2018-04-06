b317d523447d0506bff17763ea799ce409310a21
Pavan <pavanreddy301@achiever-99.local>
2018-04-06 11:27:45 +0530

    second edit

--- github/github/ViewController.swift
+++ github/github/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hai teegala")
+        print("hai teegala ashok")
         print("hai pavan")
     }
 
