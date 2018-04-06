f74890acc55ca5e44918981241abdaba925546c3
sarath <sarathchandra.mopuru@achiever-85.local>
2018-04-06 11:15:28 +0530

    welcome message

---
+++ github/github/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by sarath on 4/6/18.
+//  Copyright © 2018 com.brn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hru teegala")
+    }
+
+  
+
+}
+
